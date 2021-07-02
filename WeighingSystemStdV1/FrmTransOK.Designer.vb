61b70686a0c8501758ae6f07950ff91375573837
mfainza22 <44632597+mfainza22@users.noreply.github.com>
2021-07-02 10:53:59 +0800

    Initial Commit

---
+++ WeighingSystemStdV1/FrmTransOK.Designer.vb
@@ -0,0 +1,153 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class FrmTransOK
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(FrmTransOK))
+        Me.TxtMsg = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.BtnClose = New MykeCtrlEx.PushButton()
+        Me.BtnReprint = New MykeCtrlEx.PushButton()
+        Me.SuspendLayout()
+        '
+        'TxtMsg
+        '
+        Me.TxtMsg.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TxtMsg.Location = New System.Drawing.Point(16, 71)
+        Me.TxtMsg.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.TxtMsg.Multiline = True
+        Me.TxtMsg.Name = "TxtMsg"
+        Me.TxtMsg.ReadOnly = True
+        Me.TxtMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical
+        Me.TxtMsg.Size = New System.Drawing.Size(523, 175)
+        Me.TxtMsg.TabIndex = 0
+        Me.TxtMsg.TabStop = False
+        Me.TxtMsg.Text = "Transaction Com"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(8, 9)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(115, 20)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "Status Window"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Segoe UI Symbol", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.White
+        Me.Label2.Location = New System.Drawing.Point(16, 332)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(4, 0, 4, 0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(202, 19)
+        Me.Label2.TabIndex = 11
+        Me.Label2.Text = "Copright By: Terriniel Scales Inc."
+        '
+        'BtnClose
+        '
+        Me.BtnClose.BackgroundImage = CType(resources.GetObject("BtnClose.BackgroundImage"), System.Drawing.Image)
+        Me.BtnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.BtnClose.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnClose.FlatAppearance.BorderSize = 0
+        Me.BtnClose.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnClose.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnClose.ForeColor = System.Drawing.Color.Maroon
+        Me.BtnClose.GUI_ACCSS = "JMoiakne122208"
+        Me.BtnClose.GUI_ENABLE_HOVER = True
+        Me.BtnClose.GUI_ENABLED = True
+        Me.BtnClose.GUI_FORECOLOR_HOVER = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BtnClose.GUI_ICONS = MykeCtrlEx.PushButton.Icons.None
+        Me.BtnClose.GUI_IMAGE_TEMPLATE = MykeCtrlEx.PushButton.ButtonTemplates.SlickBlue
+        Me.BtnClose.GUI_MOUSELEAVE_FORECOLOR = System.Drawing.Color.Empty
+        Me.BtnClose.GUI_ORIENTATION = MykeCtrlEx.PushButton.Orientations.Horizontal
+        Me.BtnClose.Location = New System.Drawing.Point(413, 260)
+        Me.BtnClose.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.BtnClose.Name = "BtnClose"
+        Me.BtnClose.Size = New System.Drawing.Size(127, 58)
+        Me.BtnClose.TabIndex = 9
+        Me.BtnClose.Text = "Close"
+        Me.BtnClose.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.BtnClose.UseVisualStyleBackColor = True
+        '
+        'BtnReprint
+        '
+        Me.BtnReprint.BackgroundImage = CType(resources.GetObject("BtnReprint.BackgroundImage"), System.Drawing.Image)
+        Me.BtnReprint.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.BtnReprint.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.BtnReprint.FlatAppearance.BorderSize = 0
+        Me.BtnReprint.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.BtnReprint.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.BtnReprint.ForeColor = System.Drawing.Color.Maroon
+        Me.BtnReprint.GUI_ACCSS = "JMoiakne122208"
+        Me.BtnReprint.GUI_ENABLE_HOVER = True
+        Me.BtnReprint.GUI_ENABLED = True
+        Me.BtnReprint.GUI_FORECOLOR_HOVER = System.Drawing.Color.Empty
+        Me.BtnReprint.GUI_ICONS = MykeCtrlEx.PushButton.Icons.None
+        Me.BtnReprint.GUI_IMAGE_TEMPLATE = MykeCtrlEx.PushButton.ButtonTemplates.SlickBlue
+        Me.BtnReprint.GUI_MOUSELEAVE_FORECOLOR = System.Drawing.Color.Empty
+        Me.BtnReprint.GUI_ORIENTATION = MykeCtrlEx.PushButton.Orientations.Horizontal
+        Me.BtnReprint.Location = New System.Drawing.Point(268, 260)
+        Me.BtnReprint.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.BtnReprint.Name = "BtnReprint"
+        Me.BtnReprint.Size = New System.Drawing.Size(137, 58)
+        Me.BtnReprint.TabIndex = 8
+        Me.BtnReprint.Text = "Re-Print"
+        Me.BtnReprint.TextAlign = System.Drawing.ContentAlignment.TopLeft
+        Me.BtnReprint.UseVisualStyleBackColor = True
+        '
+        'FrmTransOK
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.WeighingSystemV1_1.My.Resources.Resources.msgbox
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(556, 366)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.BtnClose)
+        Me.Controls.Add(Me.BtnReprint)
+        Me.Controls.Add(Me.TxtMsg)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.KeyPreview = True
+        Me.Margin = New System.Windows.Forms.Padding(4, 4, 4, 4)
+        Me.Name = "FrmTransOK"
+        Me.ShowInTaskbar = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "FrmTransOK"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TxtMsg As System.Windows.Forms.TextBox
+    Friend WithEvents BtnClose As MykeCtrlEx.PushButton
+    Friend WithEvents BtnReprint As MykeCtrlEx.PushButton
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+End Class
---
+++ WeighingSystemStdV1/MOD_REPORTING.vb
@@ -0,0 +1,368 @@
+﻿Module MOD_REPORTING
+    Public Sub PrintToPrinter__OLD(ByVal ReportFile As String, ByVal SelectionFormula As String,
+                          ByVal RR As CrystalDecisions.CrystalReports.Engine.ReportDocument, Optional ByVal refno As String = "")
+        Try
+            Dim SysSettings As New settings
+            SysSettings.Load()
+            If IsNothing(RR) Then
+                RR = New CrystalDecisions.CrystalReports.Engine.ReportDocument
+                RR.Load(ReportFile)
+                'SetConnectionString(RR)
+                'RR.RecordSelectionFormula = SelectionFormula
+            End If
+
+
+            Dim dds As ds = New ds()
+            Dim outbount_tbl As String = "Select * from Outbound_tbl where refno = '" & refno & "'"
+            Dim comm_tbl As String = ""
+            Dim sup_tbl As String = ""
+            Dim cust_tbl As String = ""
+            Dim useraccounts1 As String = ""
+            Dim useraccounts2 As String = ""
+            Dim compnyProf As String = ""
+            Dim sa As New OleDb.OleDbDataAdapter
+
+
+            RR.DataDefinition.FormulaFields.Item("unitweight").Text = "'" & SysSettings.UnitWeight & "'"
+            RR.DataDefinition.FormulaFields.Item("DeductUnit").Text = "'" & SysSettings.DeductUnit & "'"
+            RR.DataDefinition.FormulaFields.Item("TicketType").Text = "'" & SysSettings.DeductUnit & "'"
+            'RR.DataDefinition.FormulaFields.Item("unitweight").Text = "'" & SysSettings.UnitWeight & "'"
+
+            If String.IsNullOrEmpty(refno).Equals(False) Then
+                Dim Src As String =
+            "SELECT " &
+            "(SELECT CUSTNAME FROM CUST_TBL WHERE CUSTCODE = CustomerCode)" &
+              ",(SELECT CUSTAddr FROM CUST_TBL WHERE CUSTCODE = CustomerCode)" &
+",(SELECT SUPNAME FROM SUP_TBL WHERE SUPCODE =SupplierCode)" &
+",(SELECT SupAddr FROM SUP_TBL WHERE SUPCODE =SupplierCode)" &
+",(SELECT COMMDESC FROM COMM_TBL WHERE COMMCODE = CommodityCode)" &
+",(SELECT PRICING FROM COMM_TBL WHERE COMMCODE = CommodityCode)" &
+",(SELECT USER_DISPLAYNAME FROM USERACCOUNTS WHERE User_id = WeigherIn),* FROM OUTBOUND_TBL where RefNo = '" & refno & "'"
+                Dim cls As New CLS_OLE_DB
+                cls.CONNECTION = OLEDBCONX
+                cls.Source = Src
+                cls.OpenSource()
+                Dim gr As String = 0
+                Dim tr As String = 0
+                Dim nt As String = 0
+                Dim dtout As String = 0
+                Dim tbl As New DataTable()
+
+                With cls.DATAREADER
+                    If .HasRows Then
+                        While .Read
+                            comm_tbl = "select * from  comm_tbl where  COMMCODE = '" & .Item("CommodityCode") & "'"
+                            sup_tbl = "select * from  sup_tbl where  supcode = '" & .Item("supplierCode") & "'"
+                            cust_tbl = "select * from  cust_tbl where  custcode = '" & .Item("customerCode") & "'"
+                            useraccounts1 = "select * from  useraccounts where  user_id = '" & .Item("WeigherIn") & "'"
+                            useraccounts2 = "select * from  useraccounts where  user_id = '" & .Item("WeigherOut") & "'"
+                            compnyProf = "select * from companyProfile"
+                        End While
+                        .Close()
+
+                        sa = New OleDb.OleDbDataAdapter(comm_tbl, OLEDBCONX)
+                        sa.Fill(dds, "Comm_Tbl")
+
+                        sa = New OleDb.OleDbDataAdapter(outbount_tbl, OLEDBCONX)
+                        sa.Fill(dds, "Outbound_tbl")
+
+
+                        sa = New OleDb.OleDbDataAdapter(sup_tbl, OLEDBCONX)
+                        sa.Fill(dds, "sup_tbl")
+
+                        sa = New OleDb.OleDbDataAdapter(cust_tbl, OLEDBCONX)
+                        sa.Fill(dds, "cust_tbl")
+
+                        sa = New OleDb.OleDbDataAdapter(useraccounts1, OLEDBCONX)
+                        sa.Fill(dds, "useraccounts")
+
+                        sa = New OleDb.OleDbDataAdapter(useraccounts2, OLEDBCONX)
+                        sa.Fill(dds, "useraccounts_1")
+
+                        sa = New OleDb.OleDbDataAdapter(compnyProf, OLEDBCONX)
+                        sa.Fill(dds, "CompanyProfile")
+                    End If
+                End With
+            End If
+
+
+            RR.SetDataSource(dds)
+            RR.PrintOptions.PrinterName = GetDefaultPrinter()
+            '  RR.SaveAs(Application.StartupPath & "\Reports\tmp.rpt")
+            'Dim margins As CrystalDecisions.Shared.PageMargins
+
+            'margins = RR.PrintOptions.PageMargins
+            'margins.bottomMargin = 0
+            'margins.leftMargin = 0
+            'margins.rightMargin = 0.0
+            'margins.topMargin = 100
+            'RR.PrintOptions.ApplyPageMargins(margins)
+
+            Dim printPrompt As New Windows.Forms.PrintDialog
+            printPrompt.PrinterSettings = New System.Drawing.Printing.PrinterSettings
+            printPrompt.AllowSomePages = True '
+            'printPrompt.PrinterSettings.PrinterName = "Send To OneNote 2013"
+            ' printPrompt.PrinterSettings.PrinterName = "Microsoft XPS Document Writer"
+
+            printPrompt.PrinterSettings.DefaultPageSettings.Margins.Bottom = 0
+            printPrompt.PrinterSettings.DefaultPageSettings.Margins.Top = 0
+            printPrompt.PrinterSettings.DefaultPageSettings.Margins.Left = 0
+            printPrompt.PrinterSettings.DefaultPageSettings.Margins.Right = 0
+            RR.Refresh()
+            RR.PrintToPrinter(printPrompt.PrinterSettings, printPrompt.PrinterSettings.DefaultPageSettings, False)
+            RR.Close()
+            RR.Dispose()
+            '  RR.PrintToPrinter(1, False, 0, 0)
+
+            'RR.SaveAs(Application.StartupPath & "\Reports\tmp.rpt")
+        Catch ex As Exception
+            Throw New Exception(ex.Message)
+        End Try
+    End Sub
+
+
+    Public Sub PrintToPrinter(ticketType As TicketTypeEnum, Optional ByVal refno As String = "")
+        Try
+            Dim SysSettings As New settings
+            SysSettings.Load()
+
+            Dim repotFile As String = Application.StartupPath & "\Reports\" & "TcktInOutAll.rpt"
+
+            Dim RR = New CrystalDecisions.CrystalReports.Engine.ReportDocument
+            RR.Load(repotFile)
+
+
+            Dim dds As ds = New ds()
+            Dim outbount_tbl As String = "Select * from Outbound_tbl where refno = '" & refno & "'"
+            Dim comm_tbl As String = ""
+            Dim sup_tbl As String = ""
+            Dim cust_tbl As String = ""
+            Dim useraccounts1 As String = ""
+            Dim useraccounts2 As String = ""
+            Dim compnyProf As String = ""
+            Dim sa As New OleDb.OleDbDataAdapter
+
+
+            RR.DataDefinition.FormulaFields.Item("unitweight").Text = "'" & SysSettings.UnitWeight & "'"
+            RR.DataDefinition.FormulaFields.Item("DeductUnit").Text = "'" & SysSettings.DeductUnit & "'"
+            RR.DataDefinition.FormulaFields.Item("TicketType").Text = "'" & ticketType.ToString() & "'"
+
+            If String.IsNullOrEmpty(refno).Equals(False) Then
+                Dim Src As String =
+            "SELECT " &
+            "(SELECT CUSTNAME FROM CUST_TBL WHERE CUSTCODE = CustomerCode)" &
+              ",(SELECT CUSTAddr FROM CUST_TBL WHERE CUSTCODE = CustomerCode)" &
+",(SELECT SUPNAME FROM SUP_TBL WHERE SUPCODE =SupplierCode)" &
+",(SELECT SupAddr FROM SUP_TBL WHERE SUPCODE =SupplierCode)" &
+",(SELECT COMMDESC FROM COMM_TBL WHERE COMMCODE = CommodityCode)" &
+",(SELECT PRICING FROM COMM_TBL WHERE COMMCODE = CommodityCode)" &
+",(SELECT USER_DISPLAYNAME FROM USERACCOUNTS WHERE User_id = WeigherIn),* FROM OUTBOUND_TBL where RefNo = '" & refno & "'"
+                Dim cls As New CLS_OLE_DB
+                cls.CONNECTION = OLEDBCONX
+                cls.Source = Src
+                cls.OpenSource()
+                Dim gr As String = 0
+                Dim tr As String = 0
+                Dim nt As String = 0
+                Dim dtout As String = 0
+                Dim tbl As New DataTable()
+
+                With cls.DATAREADER
+                    If .HasRows Then
+                        While .Read
+                            comm_tbl = "select * from  comm_tbl where  COMMCODE = '" & .Item("CommodityCode") & "'"
+                            sup_tbl = "select * from  sup_tbl where  supcode = '" & .Item("supplierCode") & "'"
+                            cust_tbl = "select * from  cust_tbl where  custcode = '" & .Item("customerCode") & "'"
+                            useraccounts1 = "select * from  useraccounts where  user_id = '" & .Item("WeigherIn") & "'"
+                            useraccounts2 = "select * from  useraccounts where  user_id = '" & .Item("WeigherOut") & "'"
+                            compnyProf = "select * from companyProfile"
+                        End While
+                        .Close()
+
+                        sa = New OleDb.OleDbDataAdapter(comm_tbl, OLEDBCONX)
+                        sa.Fill(dds, "Comm_Tbl")
+
+                        sa = New OleDb.OleDbDataAdapter(outbount_tbl, OLEDBCONX)
+                        sa.Fill(dds, "Outbound_tbl")
+
+
+                        sa = New OleDb.OleDbDataAdapter(sup_tbl, OLEDBCONX)
+                        sa.Fill(dds, "sup_tbl")
+
+                        sa = New OleDb.OleDbDataAdapter(cust_tbl, OLEDBCONX)
+                        sa.Fill(dds, "cust_tbl")
+
+                        sa = New OleDb.OleDbDataAdapter(useraccounts1, OLEDBCONX)
+                        sa.Fill(dds, "useraccounts")
+
+                        sa = New OleDb.OleDbDataAdapter(useraccounts2, OLEDBCONX)
+                        sa.Fill(dds, "useraccounts_1")
+
+                        sa = New OleDb.OleDbDataAdapter(compnyProf, OLEDBCONX)
+                        sa.Fill(dds, "CompanyProfile")
+                    End If
+                End With
+            End If
+
+
+            RR.SetDataSource(dds)
+            RR.PrintOptions.PrinterName = GetDefaultPrinter()
+            '  RR.SaveAs(Application.StartupPath & "\Reports\tmp.rpt")
+            'Dim margins As CrystalDecisions.Shared.PageMargins
+
+            'margins = RR.PrintOptions.PageMargins
+            'margins.bottomMargin = 0
+            'margins.leftMargin = 0
+            'margins.rightMargin = 0.0
+            'margins.topMargin = 100
+            'RR.PrintOptions.ApplyPageMargins(margins)
+
+            Dim printPrompt As New Windows.Forms.PrintDialog
+            printPrompt.PrinterSettings = New System.Drawing.Printing.PrinterSettings
+            printPrompt.AllowSomePages = True '
+            'printPrompt.PrinterSettings.PrinterName = "Send To OneNote 2013"
+            ' printPrompt.PrinterSettings.PrinterName = "Microsoft XPS Document Writer"
+
+            printPrompt.PrinterSettings.DefaultPageSettings.Margins.Bottom = 0
+            printPrompt.PrinterSettings.DefaultPageSettings.Margins.Top = 0
+            printPrompt.PrinterSettings.DefaultPageSettings.Margins.Left = 0
+            printPrompt.PrinterSettings.DefaultPageSettings.Margins.Right = 0
+            RR.Refresh()
+            RR.PrintToPrinter(printPrompt.PrinterSettings, printPrompt.PrinterSettings.DefaultPageSettings, False)
+            RR.Close()
+            RR.Dispose()
+            '  RR.PrintToPrinter(1, False, 0, 0)
+
+            'RR.SaveAs(Application.StartupPath & "\Reports\tmp.rpt")
+        Catch ex As Exception
+            Throw New Exception(ex.Message)
+        End Try
+    End Sub
+    Private Function GetDefaultPrinter() As String
+        Dim Result As String = ""
+
+        Dim _SEARCHER = New System.Management.ManagementObjectSearcher(
+    "Select * from  Win32_Printer")
+
+        For Each _QUERYJOB As System.Management.ManagementObject In _SEARCHER.Get()
+            If _QUERYJOB("Default") = True Then
+                Result = _QUERYJOB("Caption")
+            End If
+        Next
+        Return Result
+    End Function
+
+    Public Sub ViewReport(ByVal crviewer As CrystalDecisions.Windows.Forms.CrystalReportViewer,
+                          ByVal ReportFile As String, ByVal SelectionFormula As String,
+                          ByVal RR As CrystalDecisions.CrystalReports.Engine.ReportDocument, ByVal DTFrom As String,
+                          ByVal DTTO As String, ByVal PreparedBy As String,
+                          ByVal SortFieldTable As String, ByVal SortField As String, ByVal SortFieldOrder As CrystalDecisions.Shared.SortDirection,
+                          ByVal Groupings As String)
+        Try
+
+            Dim ConnInfo As New CrystalDecisions.Shared.ConnectionInfo
+            If IsNothing(RR) Then
+                RR = New CrystalDecisions.CrystalReports.Engine.ReportDocument
+                RR.Load(ReportFile)
+                RR.RecordSelectionFormula = SelectionFormula
+                SetConnectionString(RR)
+            End If
+
+            RR.DataDefinition.FormulaFields.Item("From").Text = "'" & DTFrom & "'"
+            RR.DataDefinition.FormulaFields.Item("To").Text = "'" & DTTO & "'"
+            RR.DataDefinition.FormulaFields.Item("PreparedBy").Text = "'" & PreparedBy & "'"
+
+            If IsNothing(SortFieldTable) = False Then
+                Dim SortFieldData As CrystalDecisions.CrystalReports.Engine.FieldDefinition
+                SortFieldData = RR.Database.Tables(SortFieldTable).Fields(SortField)
+                RR.DataDefinition.SortFields(0).Field = SortFieldData
+                If (SortFieldOrder = CrystalDecisions.Shared.SortDirection.TopNOrder) = False Then
+                    RR.DataDefinition.SortFields(0).SortDirection = SortFieldOrder
+                End If
+            End If
+
+            'If IsNothing(Groupings) = False Then
+            '    Dim GroupFieldData As CrystalDecisions.CrystalReports.Engine.GroupNa
+
+            '    MessageBox.Show(RR.DataDefinition.GroupNameFields(0).Name)
+
+            'End If
+
+            RR.Refresh()
+            crviewer.ReportSource = RR
+            If IsNothing(SelectionFormula) = False Then
+                crviewer.SelectionFormula = SelectionFormula
+            End If
+            crviewer.Refresh()
+
+        Catch ex As Exception
+            Throw New Exception(ex.Message)
+        End Try
+    End Sub
+
+
+    Private Sub SetConnectionString(ByRef Rp As CrystalDecisions.CrystalReports.Engine.ReportDocument)
+        Try
+            Dim ConStr As String = ""
+            Dim ConInfo As New CrystalDecisions.Shared.ConnectionInfo
+            Dim RpTbl As CrystalDecisions.CrystalReports.Engine.Tables = Rp.Database.Tables
+            Dim SysSettings As New settings
+            SysSettings.Load()
+
+            Select Case SysSettings.ConnectionType
+                Case "OLEDB"
+                    With ConInfo
+                        .DatabaseName = SysSettings.access_file_path
+                        .Password = SysSettings.access_pwd
+                        .AllowCustomConnection = True
+
+                    End With
+                Case "SQL"
+                    With ConInfo
+                        .ServerName = SysSettings.sql_server_name
+                        .DatabaseName = SysSettings.sql_server_catalog
+                        .UserID = SysSettings.sql_server_uid
+                        .Password = SysSettings.sql_server_pwd
+                    End With
+            End Select
+
+
+            For Each tbl As CrystalDecisions.CrystalReports.Engine.Table In RpTbl
+                Dim tblLogInfo As CrystalDecisions.Shared.TableLogOnInfo = tbl.LogOnInfo
+                tbl.LogOnInfo.ConnectionInfo = ConInfo
+                tbl.ApplyLogOnInfo(tblLogInfo)
+            Next
+
+
+            If SysSettings.ConnectionType = "SQL" Then
+                Dim sqlConnInfo As System.Data.SqlClient.SqlConnectionStringBuilder = New System.Data.SqlClient.SqlConnectionStringBuilder(SQLCONX.ConnectionString)
+                For Each connection As CrystalDecisions.CrystalReports.Engine.InternalConnectionInfo In Rp.DataSourceConnections
+                    If sqlConnInfo.IntegratedSecurity Then
+                        connection.SetConnection(sqlConnInfo.DataSource, sqlConnInfo.InitialCatalog, True)
+
+                    Else
+                        connection.SetConnection(sqlConnInfo.DataSource, sqlConnInfo.InitialCatalog, sqlConnInfo.UserID, sqlConnInfo.Password)
+                        connection.IntegratedSecurity = False
+                    End If
+                Next
+            End If
+
+
+        Catch ex As Exception
+            Throw New Exception(ex.Message)
+        End Try
+
+
+    End Sub
+
+
+    Public Sub LoadReportDoc(ByRef reportfile As String, ByRef MyReport As CrystalDecisions.CrystalReports.Engine.ReportDocument)
+
+        MyReport.Load(reportfile)
+        SetConnectionString(MyReport)
+    End Sub
+End Module
+
+Module _PROC
+
+End Module
